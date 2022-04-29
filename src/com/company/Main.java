d859a34a2d0429e2431fc87d7191708e46fdaff3
DrPhannibal <mahdisouri0@gmail.com>
2022-04-30 00:28:01 +0430

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        Scanner sc=new Scanner(System.in);
+
+//        System.out.println("plz enter a number");
+//        int number_1=sc.nextInt();
+//        System.out.println(number_1);
+
+//        System.out.println("plz enter char");
+//        char var_ch=sc.next().charAt(1);
+//        System.out.println(var_ch);
+
+//        System.out.println("plz enter String");
+//        String var_st=sc.nextLine();
+//        System.out.println(var_st);
+
+        System.out.println("plz enter boolean");
+        boolean var_bool=sc.nextBoolean();
+        System.out.println(var_bool);
+    }
+}
